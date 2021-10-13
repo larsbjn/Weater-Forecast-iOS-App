3b0073c0328f7072d6648c7857b0918d610cb5f4
Lars Jørgensen <lars.b.jn@gmail.com>
2021-10-14 00:06:29 +0200

    initial

---
+++ Weather Forecast/Weather Forecast/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather Forecast
+//
+//  Created by Lars Jørgensen on 13/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather Forecast/Weather Forecast/Weather_ForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ForecastApp.swift
+//  Weather Forecast
+//
+//  Created by Lars Jørgensen on 13/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
