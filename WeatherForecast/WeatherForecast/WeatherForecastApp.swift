b45151ac857ebc7835b1e903b80cd711c15400d5
Lars Jørgensen <lars.b.jn@gmail.com>
2021-10-14 00:39:02 +0200

    :tada: Initial project
    OCD sort of fixed, new project. Let's go!

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Lars Jørgensen on 14/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
