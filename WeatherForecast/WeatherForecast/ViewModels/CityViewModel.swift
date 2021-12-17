d1764d522cd4742ce9636aeb0daa150d1410eee3
Lars Jørgensen <lars.b.jn@gmail.com>
2021-12-17 18:46:21 +0100

    Added Core Data

--- WeatherForecast/WeatherForecast/ViewModels/CityViewModel.swift
+++ WeatherForecast/WeatherForecast/ViewModels/CityViewModel.swift
@@ -7,20 +7,26 @@
 
 import Foundation
 import SwiftUI
+import CoreData
 
 class CityViewModel : ObservableObject {
     
-    @Published var cities: [City] = { () -> [City] in
-        if let objects = UserDefaults.standard.value(forKey: "cities") as? Data {
-            let decoder = JSONDecoder()
-            if let objectsDecoded = try? decoder.decode(Array.self, from: objects) as [City] {
-                return objectsDecoded
-            }
-        }
-        return []
-    }()
+    @Published var cities: [City] = []
+    
+    let context = PersistenceController.shared.container.viewContext
+    var citiesEntities: [CityEntity] = []
     
     init() {
+        let request = NSFetchRequest<CityEntity>(entityName: "CityEntity")
+        
+        guard let fetchedCities = try? context.fetch(request) else { return }
+        self.citiesEntities = fetchedCities
+        
+        for city in citiesEntities {
+            let newCoord = Coord(lat: city.coord?.lat ?? 0, lon: city.coord?.lon ?? 0)
+            cities.append(City(name: city.name ?? "", coord: newCoord))
+        }
+        
         let locationManager = LocationManager()
         locationManager.setOnLocationGranted(action: {
             guard let location = locationManager.getLocation() else { return }
@@ -42,19 +48,39 @@ class CityViewModel : ObservableObject {
         CallFindCityAPI(name: name, action: { cityList in
             guard let city = cityList.list.first else { return }
             self.cities.append(city)
-            self.saveAllCities()
+            
+            self.addCityEntity(city: city)
         })
+        
+    }
+    
+    func addCityEntity(city: City) {
+        let newCoord = CoordEntity(context: context)
+        newCoord.lon = city.coord.lon
+        newCoord.lat = city.coord.lat
+        
+        let newCity = CityEntity(context: context)
+        newCity.name = city.name
+        newCity.coord = newCoord
+        
+        citiesEntities.append(newCity)
+        
+        try? context.save()
     }
     
     func deleteCity(_ index: IndexSet) -> Void {
+        guard let i = index.first else { return }
+        deleteCityEntity(city: cities[i])
         cities.remove(atOffsets: index)
-        saveAllCities()
     }
     
-    func saveAllCities() {
-        let encoder = JSONEncoder()
-        if let encoded = try? encoder.encode(cities.filter({ !$0.isGps })){
-            UserDefaults.standard.set(encoded, forKey: "cities")
+    func deleteCityEntity(city: City) {
+        for c in citiesEntities {
+            if c.name == city.name {
+                context.delete(c)
+                try? context.save()
+                return
+            }
         }
     }
 }
